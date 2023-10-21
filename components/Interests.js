b97263d0c37245a977362165193d0196f6e992f6
Gabriel Santos Teixeira <69646007+Gabrielsst@users.noreply.github.com>
2023-10-21 09:35:52 -0300

    Adicionando projeto

---
+++ components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+    return (
+        <div className="div--interests container--content">
+            <h2 className="title">Interests</h2>
+            <p className="text">I love lasagna. Like to draw things. Player, but not Pro. Internet fanatic. Enthusiasts. Travel is one of my passions. iron man's fan. Coffee fanatic.</p>
+        </div>
+    )
+}
